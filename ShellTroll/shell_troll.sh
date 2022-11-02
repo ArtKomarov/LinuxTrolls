1f273938a5ed980f0cf447a1a763076702b4c12d
ArtKomarov <Komarov.A@phystech.edu>
2022-11-03 00:00:32 +0100

    Add --include-preparations bash option

--- ShellTroll/shell_troll.sh
+++ ShellTroll/shell_troll.sh
@@ -3,8 +3,11 @@
 path=$(dirname "$0")
 
 # Preparations
-#g++ -c shell_troll.cpp -Wall -Wextra -fPIC
-#g++ -shared -o libshell_troll.so shell_troll.o
+if [ "$1" == "--include-preparations" ]
+then
+    g++ -c shell_troll.cpp -Wall -Wextra -fPIC
+    g++ -shared -o libshell_troll.so shell_troll.o
+fi
 
 # Compile
 g++ -Wall -Wextra $path/main.cpp -o main.out -L $path -lshell_troll
