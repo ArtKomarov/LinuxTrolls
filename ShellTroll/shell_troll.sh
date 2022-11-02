3532a17ddf95ccc34bff51a76f3b3b88ba9b0e40
ArtKomarov <Komarov.A@phystech.edu>
2022-11-03 01:22:49 +0300

    Comment out preparations

--- ShellTroll/shell_troll.sh
+++ ShellTroll/shell_troll.sh
@@ -3,9 +3,8 @@
 path=$(dirname "$0")
 
 # Preparations
-g++ -c shell_troll.cpp -Wall -Wextra -fPIC
-
-g++ -shared -o libshell_troll.so shell_troll.o
+#g++ -c shell_troll.cpp -Wall -Wextra -fPIC
+#g++ -shared -o libshell_troll.so shell_troll.o
 
 # Compile
 g++ -Wall -Wextra $path/main.cpp -o main.out -L $path -lshell_troll
